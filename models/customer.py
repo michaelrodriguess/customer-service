f8f1e807e7d50f384b3c447f4564452fa9418994
Raph2ll <raphael.ph2ll@gmail.com>
2024-12-22 13:22:51 -0300

    feat: Customer model

---
+++ models/customer.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
+from typing import Optional
+import ulid
+
+class Customer(BaseModel):
+    id: str = Field(default_factory=lambda: str(ulid.new()))
+    name: str
+    email: EmailStr
+    created_at: datetime = Field(default_factory= datetime.now)
+    updated_at: datetime = Field(default=None)
