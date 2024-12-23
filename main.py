1eb44418ee31da1b3f7d9fdd68e6201e0884ba92
Michael Rodrigues <run.michael97@gmail.com>
2024-12-23 20:56:25 -0300

    feat: create architecture and add migrations to db

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
+
+@app.get("/")
+def index(request: Request):
+    return {"message": "Faith in god and children."}
