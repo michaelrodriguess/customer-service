0e95c4472f67a1a41ea0b733de39cd89f01aa35a
Raph2ll <raphael.ph2ll@gmail.com>
2024-12-24 16:50:12 -0300

    feat: add database services for fetching customers

---
+++ services/customer_service.py
@@ -0,0 +1,20 @@
+import logging
+from models.customer import Customer
+from storages.customer_storage import CustomerStorage
+from typing import List
+
+class CustomerService:
+    
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.storage = CustomerStorage()
+
+    def get_customer_by_id(self, id: str) -> Customer:
+        self.logger.info(f"Getting customer by id...") 
+        return self.storage.get_customer_by_id(id)
+        
+    def get_all_customers(self) -> List[Customer]:
+        self.logger.info(f"Getting all customers...") 
+        return self.storage.get_all_customers()
+        
+    
