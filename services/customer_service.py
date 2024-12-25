a65902f124fea676fd3b5e556058892ef642d441
Hesfra <contato.chfreire@gmail.com>
2024-12-25 05:05:41 -0300

    corrigindo a service

--- services/customer_service.py
+++ services/customer_service.py
@@ -5,16 +5,16 @@ from models.customer import Customer, Customer_update
 class Customer_service:
 
     def __init__(self):
-        self.storage = CustomerStorage()
+        self.config = CustomerStorage()
 
     def update_customer(self, id: str, customer: Customer):
 
-        updated_customer = self.storage.update_customer(id, customer)
+        updated_customer = self.config.update_customer(id, customer)
 
         return updated_customer
 
     def patch_customer(self, id: str, customer: Customer_update):
 
-        updated_customer = self.storage.patch_customer(id, customer)
+        updated_customer = self.config.patch_customer(id, customer)
 
         return updated_customer
