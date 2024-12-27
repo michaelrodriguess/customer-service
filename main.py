7d5f33b70530760da815f0c12b99910d86d2c5ec
Raph2ll <raphael.ph2ll@gmail.com>
2024-12-27 12:07:25 -0300

    feat: include customer_router

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from routes.customer_router import router as customer_router
+import logging
 
+logger = logging.getLogger(__name__)
 app = FastAPI()
 
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
+
+app.include_router(customer_router)
