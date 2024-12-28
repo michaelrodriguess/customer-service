df0abac168689862f5d4ddf4d90eb09fe594280c
Raphael <Raphael.ph2ll@gmail.com>
2024-12-28 18:56:16 -0300

    remove: router database handler

--- routes/customer_router.py
+++ routes/customer_router.py
@@ -12,11 +12,7 @@ customer_service = CustomerService()
 
 @router.get("/customers", response_model=List[Customer])
 def get_all_customers():
-    try:
-        return customer_service.get_all_customers() 
-    except DatabaseError as ex:
-        logger.warning(f"Failed to get Customer: {ex}")
-        raise HTTPException(status_code=500, detail=str(ex))
+    return customer_service.get_all_customers()
 
 @router.get("/customers/{id}", response_model=Customer)
 def get_customer_by_id(id: str):
@@ -24,7 +20,4 @@ def get_customer_by_id(id: str):
         return customer_service.get_customer_by_id(id)
     except ValueError as ex:
         logger.warning(f"Customer not found: {ex}")
-        raise HTTPException(status_code=404, detail=str(ex))
-    except DatabaseError as ex:
-        logger.warning(f"Failed to get Customer: {ex}")
-        raise HTTPException(status_code=500, detail=str(ex))
+        raise HTTPException(status_code=404, detail=ex.message)
