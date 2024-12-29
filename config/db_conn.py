28d61d06fc804cbf7de267889b6d0b01f32815a0
Raphael <Raphael.ph2ll@gmail.com>
2024-12-29 18:26:38 -0300

    fix: rename db_conn

--- config/db_conn.py
+++ config/db_conn.py
@@ -4,12 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db_conn = psycopg2.connect(
+conn = psycopg2.connect(
     database=os.getenv("DATABASE_NAME"),
     host=os.getenv("DATABASE_HOST"),
     user=os.getenv("DATABASE_USER"),
     password=os.getenv("DATABASE_PASSWORD"),
     port=os.getenv("DATABASE_PORT"),
 )
-
-conn = db_conn
