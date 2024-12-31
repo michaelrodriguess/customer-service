f3557734a7aa287e5df4694aac0836b19de56c03
Raph2ll <raphael.ph2ll@gmail.com>
2024-12-31 16:04:32 -0300

    feat: main.py lifespan

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends
 from routes.customer_router import get_customer_service
+from configs.db_conn import get_database_connection
+from services.customer_service import Customer_service
+from storages.customer_storage import CustomerStorage
 import logging
 from routes.customer_router import router
 
 logger = logging.getLogger(__name__)
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    db_connection = get_database_connection()
+    customer_storage = CustomerStorage(db_connection=db_connection)
+    customer_service = Customer_service(customer_storage)
+    
+    yield {"customer_service": customer_service}
+    logger.info(f"Shutdown application")
+    
+app = FastAPI(lifespan=lifespan, title="Customer Service",)
 
 app.include_router(
     router,
