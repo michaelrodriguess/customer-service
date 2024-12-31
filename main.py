f7c933ce86ec80121e889b3525ba48ba45a6f173
Raph2ll <raphael.ph2ll@gmail.com>
2024-12-31 12:53:12 -0300

    feat: router dependencies

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from routes.customer_service_provider import get_customer_service
 import logging
 from routes.customer_router import customer_router
 
 logger = logging.getLogger(__name__)
 app = FastAPI()
 
-app.include_router(customer_router)
-
+app.include_router(
+    customer_router,
+    dependencies=[Depends(get_customer_service)]
+)
 
 @app.get("/health")
 def health_check():
