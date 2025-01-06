b54bae2d072a01bafc721c1af17665274f75b904
Raph2ll <raphael.ph2ll@gmail.com>
2025-01-06 16:11:23 -0300

    test: storage error cases

--- tests/test_customer_storage.py
+++ tests/test_customer_storage.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from storages.customer_storage import CustomerStorage
 from models.customer_model import Customer
 import pytest
+from psycopg2 import DatabaseError
 
 @pytest.fixture
 def customer_row():
@@ -31,6 +32,15 @@ def test_get_customer_by_id(mock_cursor, storage, customer, customer_row):
         ("01F8MECHZX3TBDSZ7XD96VR2H5",),
     )
 
+def test_get_customer_by_id_value_error(mock_cursor, storage):
+    mock_cursor.fetchone.return_value = None
+
+    with pytest.raises(ValueError):
+        storage.get_customer_by_id("01JFTE35ZRRZWCSKK6TBB1DZCT")
+
+    mock_cursor.execute.assert_called_once()
+    mock_cursor.fetchone.assert_called_once()
+
 def test_get_all_customer_success(mock_cursor, storage, customer, customer_row):
     mock_cursor.fetchall.return_value = [customer_row]
     
@@ -45,3 +55,12 @@ def test_get_all_customer_success(mock_cursor, storage, customer, customer_row):
         WHERE active = true;
         """
     )
+
+def test_get_all_customers_database_error(mock_cursor, storage):
+    mock_cursor.execute.side_effect = DatabaseError()
+
+    with pytest.raises(DatabaseError):
+        storage.get_all_customers()
+
+    mock_cursor.execute.assert_called_once()
+    mock_cursor.fetchall.assert_not_called()
